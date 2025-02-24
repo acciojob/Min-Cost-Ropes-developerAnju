52e419f3d211e7b9cd33cbbee1473f4f6309e539
developerAnju <116105555+developerAnju@users.noreply.github.com>
2025-02-25 00:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function mincost(arr)
 { 
 //write your code here
+	if(arr.length === 1) return 0;
+	const heap = new MinHeap();
+	for (let length of arr){
+		heap.insert(length);
+	}
+	let totalCost = 0;
+	while(heap.size() > 1){
+		const first = heap.extractMin();
+		const second = heap.extractMin();
+
+		const cost = first + second;
+		totalCost += cost;
+
+		heap.insert(cost);
+	}
+	return totalCost;
 // return the min cost
   
 }
+
 
 module.exports=mincost;
